40b39e67a259202351efab5010ea9bab0a4aa541
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-02 00:40:26 +0100

    Added a gold background to the #box

--- Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
@@ -1,7 +1,8 @@
 ﻿$(document).ready(function () {
     $("#box").width(450).height(400).addClass("center-block").css({
         "margin-top": "5%",
-        "border": "1px solid #000000"
+        "border": "1px solid #000000",
+        "background": "gold"
     });
     $("#box-list").addClass("list-unstyled").css({
         "text-align": "center",
