ad2b709e05d59303ede4d7ccea7855785b0e7255
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-01 23:59:27 +0100

    Added font-size and font-family to the <ul>

--- Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
@@ -1,4 +1,8 @@
 ﻿$(document).ready(function () {
     $("#box").width(400).height(400).addClass("center-block");
-    $("#box-list").addClass("list-unstyled");
+    $("#box-list").addClass("list-unstyled").css({
+        "margin": "auto",
+        "font-size": "15px",
+        "font-family": "Verdana"
+    });
 });
