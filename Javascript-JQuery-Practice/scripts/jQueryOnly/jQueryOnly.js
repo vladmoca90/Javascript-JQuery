16256af96f8d433ab955440c4c1389e7fec4602e
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-02 00:34:27 +0100

    Added a 1px black border to the #box

--- Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     $("#box").width(400).height(400).addClass("center-block").css({
-        "margin-top": "5%"
+        "margin-top": "5%",
+        "border": "1px solid #000000"
     });
     $("#box-list").addClass("list-unstyled").css({
         "text-align": "center",
