fc388e45790edf912489fa18fc2cf5cf331395b4
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-04 16:03:53 +0100

    Added some space between the codes in the jQueryOnly.js file

--- Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnly/jQueryOnly.js
@@ -1,23 +1,30 @@
 ﻿$(document).ready(function () {
+
     $("#box").width(450).height(400).addClass("center-block").css({
         "margin-top": "5%",
         "border": "1px solid #000000",
         "background": "gold"
     });
+
     $("#box-list").addClass("list-unstyled").css({
         "text-align": "center",
         "font-size": "16px",
         "font-family": "Verdana"
     });
+
     $("#box-list").children("li.first").css("font-weight", "bold");
+
     $("#box-list").children("li.second").css({
         "font-style": "italic",
         "color": "#FF0000"
     });
+
     $("#box-list").children("li").not("li.second").click(function () {
         $(this).css("color", "blue");
     });
+
     $("#box-list>li").css({
         "padding-top": "10px"
     });
+
 });
