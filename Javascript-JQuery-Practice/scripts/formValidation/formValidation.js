010427d5940a96ed269e08f6bfe5ea120794bda7
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-26 19:10:10 +0100

    Added a function to validate the email address

--- Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
+++ Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
@@ -15,3 +15,19 @@ function lastName() {
         document.getElementsByTagName("span")[0].innerHTML = "";
     }
 }
+
+function mail() {
+    var emailValid = document.getElementsByName("validEmail")[0].value;
+    if (
+        emailValid.indexOf("@") <= 0 ||
+        emailValid.indexOf("@") != emailValid.lastIndexOf("@") ||
+        emailValid.lastIndexOf(".") <= (emailValid.indexOf("@") + 2) ||
+        emailValid.lastIndexOf(".") > (emailValid.length - 3) ||
+        emailValid.lastIndexOf(".") < (emailValid.length - 5) ||
+        emailValid.indexOf(" ") >= 0
+    ) {
+        document.getElementsByTagName("span")[1].innerHTML = "Email is not valid!";
+    } else {
+        document.getElementsByTagName("span")[1].innerHTML = "";
+    }
+}
