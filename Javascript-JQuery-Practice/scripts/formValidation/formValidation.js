a9bda95c9fc8abdaa6a858b98c681ac53178ac77
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-26 18:32:31 +0100

    Created a function that verifies the validity of the firstName

--- Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
+++ Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
@@ -1,3 +1,8 @@
-﻿function formValidation() {
-
+﻿function firstName() {
+    var firstN = document.getElementsByName("firstName")[0].value;
+    if (firstN.length < 3) {
+        document.getElementsByTagName("span")[0].innerHTML = "Your first name is too short!";
+    } else {
+        document.getElementsByTagName("span")[0].innerHTML = "";
+    }
 }
