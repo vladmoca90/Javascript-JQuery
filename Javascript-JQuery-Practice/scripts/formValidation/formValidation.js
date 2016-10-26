a6be6dce00876d673259c7317b49d86d7c20c201
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-26 18:53:01 +0100

    Created a function for the validation of the last name

--- Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
+++ Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
@@ -6,3 +6,12 @@
         document.getElementsByTagName("span")[0].innerHTML = "";
     }
 }
+
+function lastName() {
+    var lastN = document.getElementsByName("lastName")[0].value;
+    if (lastN.length < 3) {
+        document.getElementsByTagName("span")[0].innerHTML = "Your last name is too short!";
+    } else {
+        document.getElementsByTagName("span")[0].innerHTML = "";
+    }
+}
