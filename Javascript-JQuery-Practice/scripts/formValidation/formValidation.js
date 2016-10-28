91052512ff417ce65577384088a5c2a51365b8c0
Vlad Mocanu <vladmoca90@gmail.com>
2016-10-28 10:35:30 +0100

    Added the Javascript code for the age validation

--- Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
+++ Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
@@ -59,3 +59,13 @@ function passwrd() {
         document.getElementsByTagName("span")[3].innerHTML = "";
     }
 }
+
+function ageValidation() {
+
+    var age = document.getElementsByName("age")[0].value;
+
+    if (age >= 18) { document.getElementsByTagName("span")[2].innerHTML = "Good! You are over 18."; }
+
+    else { document.getElementsByTagName("span")[2].innerHTML = "You must be over 18 to complete the form!"; }
+    ;
+}
