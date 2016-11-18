886916f6366b4a3ca8edcaac7d180ee5107833a6
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-18 13:30:45 +0000

    Added cursor: pointer to the "td"

--- Javascript-JQuery-Practice/scripts/jQueryOnlyTable/jQueryOnlyTable.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnlyTable/jQueryOnlyTable.js
@@ -1,8 +1,9 @@
 ﻿$(document).ready(function () {
     $("table").width(450).addClass("table table-hover").css("margin", "auto");
+    $("td").css("cursor", "pointer");
     $("th, td").css({
         "text-align": "center",
         "font-size": "15px",
-        "font-family": "Tahoma"
+        "font-family": "Tahoma",
     });
 });
