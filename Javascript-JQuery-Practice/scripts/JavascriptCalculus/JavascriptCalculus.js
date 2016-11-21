16853823cace67f57da98ce5e9b4a48e71f70a56
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-21 16:03:17 +0000

    Added some space between variables x, y and z

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
@@ -4,10 +4,13 @@ x.toFixed(5) = 13.73456;
 
 x.toPrecision(8) = 13.734564;
 
+
 var y = "Vlad Mocanu is a Front-End Developer";
 
 y.indexOf("a") = 2;
 
 y.lastIndexOf("e") = 34;
 
+
 var z = ["Ana Maria", 2, "Vlad", 7, 1015, "75", "New York City"];
+
