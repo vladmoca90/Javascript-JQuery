9633617ba24d2b3f58acddc7687996b2507de952
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-21 23:58:09 +0000

    Created the xVariable() code

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
@@ -1,5 +1,10 @@
 ﻿var x = 13.348938684;
-docuent.getElementById("x-number")
+document.getElementById("x-number").innerHTML = x;
+
+function xVariable() {
+    x.toFixed(6);
+    document.getElementById("x-number").innerHTML = x;
+}
 
 var z = ["Ana Maria", 2, "Vlad", 7, 1015, "75", "New York City"];
 document.getElementById("z-array").innerHTML = z;
