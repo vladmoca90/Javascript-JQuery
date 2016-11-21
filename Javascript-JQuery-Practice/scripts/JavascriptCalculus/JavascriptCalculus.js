57fbf4528cde3e8efb76cfd96e45aaa84df803fc
Vlad Mocanu <vladmoca90@gmail.com>
2016-11-21 22:54:55 +0000

    Deleted the rest, left the var z and its code only

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
@@ -1,22 +1,8 @@
-﻿var x = 13.734563785675632;
+﻿var z = ["Ana Maria", 2, "Vlad", 7, 1015, "75", "New York City"];
 
-x.toFixed(5) = 13.73456;
-
-x.toPrecision(8) = 13.734564;
-
-
-var y = "Vlad Mocanu is a Front-End Developer";
-
-y.indexOf("a") = 2;
-
-y.lastIndexOf("e") = 34;
-
-
-var z = ["Ana Maria", 2, "Vlad", 7, 1015, "75", "New York City"];
-
-document.getElementById("z-array").innerHTML = random;
+document.getElementById("z-array").innerHTML = z;
 
 function zVariable() {
-    fruits.splice(2, 2, "Mimi");
-    document.getElementById("z-array").innerHTML = random;
+    z.splice(2, 2, "Mimi");
+    document.getElementById("z-array").innerHTML = z;
 }
