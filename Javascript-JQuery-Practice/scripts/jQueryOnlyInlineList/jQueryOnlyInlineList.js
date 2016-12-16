6c4a10de1bac5d57a4e6719138985a4e7871b8c9
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-16 23:47:21 +0000

    Added margin: auto and top of 2%, font-size: 16px and a width of 500px to the <table>

--- Javascript-JQuery-Practice/scripts/jQueryOnlyInlineList/jQueryOnlyInlineList.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnlyInlineList/jQueryOnlyInlineList.js
@@ -1,3 +1,7 @@
 ﻿$(document).ready(function () {
-    $("table").addClass("table table-bordered");
+    $("table").addClass("table table-bordered").css({
+        "margin": "2% auto",
+        "width": "500px",
+        "font-size": "16px"
+    });
 });
