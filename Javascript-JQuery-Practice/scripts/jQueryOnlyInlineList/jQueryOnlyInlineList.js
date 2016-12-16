459a1d1ee62764f74f18c30c1301a22c38a58e88
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-16 23:54:20 +0000

    Added a font-size: 18px to the <ul>

--- Javascript-JQuery-Practice/scripts/jQueryOnlyInlineList/jQueryOnlyInlineList.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnlyInlineList/jQueryOnlyInlineList.js
@@ -1,5 +1,9 @@
 ﻿$(document).ready(function () {
-    $("ul").addClass("list-unstyled list-inline");
+
+    $("ul").addClass("list-unstyled list-inline").css({
+        "font-size": "18px",
+        "margin-left": "10px"
+    });
 
     $("table").addClass("table table-bordered").css({
         "margin": "2% auto",
@@ -7,4 +11,5 @@
         "font-size": "16px",
         "text-align": "center"
     });
+
 });
