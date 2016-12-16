c18ca6f49efeb19f261daf48f864e461bea3ee15
Vlad Mocanu <vladmoca90@gmail.com>
2016-12-16 23:52:02 +0000

    Added "list-unstyled list-inline" to the <ul>

--- Javascript-JQuery-Practice/scripts/jQueryOnlyInlineList/jQueryOnlyInlineList.js
+++ Javascript-JQuery-Practice/scripts/jQueryOnlyInlineList/jQueryOnlyInlineList.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(function () {
+    $("ul").addClass("list-unstyled list-inline");
+
     $("table").addClass("table table-bordered").css({
         "margin": "2% auto",
         "width": "500px",
