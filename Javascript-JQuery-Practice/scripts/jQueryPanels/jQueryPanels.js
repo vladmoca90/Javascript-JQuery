458708815ed55f8aed65001ce84031f60face22c
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-11 19:00:10 +0000

    Deleted all the js code for the panels

--- Javascript-JQuery-Practice/scripts/jQueryPanels/jQueryPanels.js
+++ Javascript-JQuery-Practice/scripts/jQueryPanels/jQueryPanels.js
@@ -1,9 +1,1 @@
 ﻿$("#main").width(500).addClass("center-block");
-
-$("#main").addClass("panel panel-default");
-
-$("#main>div:first-child").addClass("panel-heading");
-
-$("div>h2").addClass("panel-title");
-
-$("h2+div").addClass("panel-body");
