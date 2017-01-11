4ca2ef77b8e981e6c9ffea9e9f079378ed39da63
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-11 23:16:46 +0000

    Added font Andalus for .two and changed the font-size to all of them

--- Javascript-JQuery-Practice/scripts/jQueryTexts/jQueryTexts.js
+++ Javascript-JQuery-Practice/scripts/jQueryTexts/jQueryTexts.js
@@ -7,17 +7,17 @@
 
     $(".one").css({
         "font-family": "Tahoma",
-        "font-size": "15px"
+        "font-size": "16px"
     });
 
     $(".two").css({
-        "font-family": "Verdana",
-        "font-size": "15px"
+        "font-family": "Andalus",
+        "font-size": "18px"
     });
 
     $(".three").css({
         "font-family": "Calibri",
-        "font-size": "15px"
+        "font-size": "17px"
     });
 
 });
