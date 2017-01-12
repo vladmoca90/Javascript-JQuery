277d61a497858b5afd7b6f0b83466d82a5e9c30c
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-12 01:06:32 +0000

    Added a black border to the #main

--- Javascript-JQuery-Practice/scripts/jQueryTexts/jQueryTexts.js
+++ Javascript-JQuery-Practice/scripts/jQueryTexts/jQueryTexts.js
@@ -2,7 +2,8 @@
 
     $("#main").width(1000).css({
         "padding": "10px",
-        "margin": "2% auto"
+        "margin": "2% auto",
+        "border" : "1px solid #000"
     });
 
     $(".one").css({
