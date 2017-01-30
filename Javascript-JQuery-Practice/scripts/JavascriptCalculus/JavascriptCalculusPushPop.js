eb7e04cb89cdb8871960ce00b70f6e029bfe508f
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-30 19:14:48 +0000

    JavascriptCalculusPushPop.js is empty now

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusPushPop.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusPushPop.js
@@ -1,11 +1,1 @@
-﻿var fruits = ["Banana", "Orange", "Apple", "Mango", "Apricot", "Fig"];
-
-document.getElementById("practice").innerHTML = fruits;
-
-function myFunction() {
-
-    fruits.splice(1, 1, "Kiwi");
-
-    document.getElementById("practice").innerHTML = fruits;
-
-}
+﻿
