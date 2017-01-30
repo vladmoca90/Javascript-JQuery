ad9418703a66909d3fd52b5c61f40b5cc699d343
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-30 21:24:35 +0000

    Added the functions on the buttons

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusPushPop.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusPushPop.js
@@ -1,5 +1,7 @@
 ﻿var asianCountries = ["Japan", "Taiwan", "South Korea", "Vietnam", "Thailand"];
 
+document.getElementById("#countries") = asianCountries;
+
 function asianCountriesPop() {
 
     asianCountries.pop();
