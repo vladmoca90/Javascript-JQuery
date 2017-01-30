1713aa7d8e7f727faa21dad3af744d19a7bbd66c
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-30 11:44:14 +0000

    Added some spaces between the codes

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculus.js
@@ -1,7 +1,11 @@
 ﻿var fruits = ["Banana", "Orange", "Apple", "Mango"];
+
 document.getElementById("practice").innerHTML = fruits;
 
 function myFunction() {
+
     fruits.unshift("Kiwi");
+
     document.getElementById("practice").innerHTML = fruits;
+
 }
