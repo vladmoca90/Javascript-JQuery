7c62c38385927dfbacf2fb49c98e5f5ba97bd4fc
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-30 18:09:36 +0000

    Created "JavascriptCalculusPushPop" as html and js files

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusPushPop.js
@@ -0,0 +1,11 @@
+﻿var fruits = ["Banana", "Orange", "Apple", "Mango", "Apricot", "Fig"];
+
+document.getElementById("practice").innerHTML = fruits;
+
+function myFunction() {
+
+    fruits.splice(1, 1, "Kiwi");
+
+    document.getElementById("practice").innerHTML = fruits;
+
+}
