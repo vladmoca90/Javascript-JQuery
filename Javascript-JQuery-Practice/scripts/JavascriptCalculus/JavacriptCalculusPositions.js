e75eb70030227030414f46529076d46e8ad4bbbd
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-31 13:13:13 +0000

    Added a code in which shows the africanCountries[2] value

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositions.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositions.js
@@ -1,1 +1,12 @@
-﻿
+﻿var africanCountries = ["South Africa", "Zimbabwe", "Nigeria", "Morocco", "Ethiopia"];
+
+document.getElementById("african").innerHTML = africanCountries;
+
+function africanCountries() {
+
+    africanCountries[2] = africanCountries;
+
+    document.getElementById("african").innerHTML = africanCountries;
+
+}
+
