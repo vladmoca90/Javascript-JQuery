b9e18de9f7097f544c0b773e143523ea069325b3
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-31 15:17:50 +0000

    Left the africanCountries in JavacriptCalculusPositionsOne.js and the euroCountries in JavacriptCalculusPositionsTwo.js

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsOne.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsOne.js
@@ -8,13 +8,3 @@ function AfricanCountries() {
 
 }
 
-var europeanCountries = ["Spain", "Greece", "Austria", "Netherlands", "Poland"];
-
-document.getElementById("european").innerHTML = europeanCountries;
-
-function EuroCountries() {
-
-    document.getElementById("european").innerHTML = europeanCountries[0];
-
-}
-
--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsTwo.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsTwo.js
@@ -1,14 +1,4 @@
-﻿var africanCountries = ["South Africa", "Zimbabwe", "Nigeria", "Morocco", "Ethiopia"];
-
-document.getElementById("african").innerHTML = africanCountries;
-
-function AfricanCountries() {
-
-    document.getElementById("african").innerHTML = africanCountries[2];
-
-}
-
-var europeanCountries = ["Spain", "Greece", "Austria", "Netherlands", "Poland"];
+﻿var europeanCountries = ["Spain", "Greece", "Austria", "Netherlands", "Poland"];
 
 document.getElementById("european").innerHTML = europeanCountries;
 
