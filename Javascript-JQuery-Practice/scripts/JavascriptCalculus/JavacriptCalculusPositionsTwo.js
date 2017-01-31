2da15bb895ba6412fa4821ad8c70f33498d7cb84
Vlad Mocanu <vladmoca90@gmail.com>
2017-01-31 15:16:57 +0000

    Created JavacriptCalculusPositionsTwo.js and JavacriptCalculusPositionsOne.js to separate the two codes

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsTwo.js
@@ -0,0 +1,20 @@
+﻿var africanCountries = ["South Africa", "Zimbabwe", "Nigeria", "Morocco", "Ethiopia"];
+
+document.getElementById("african").innerHTML = africanCountries;
+
+function AfricanCountries() {
+
+    document.getElementById("african").innerHTML = africanCountries[2];
+
+}
+
+var europeanCountries = ["Spain", "Greece", "Austria", "Netherlands", "Poland"];
+
+document.getElementById("european").innerHTML = europeanCountries;
+
+function EuroCountries() {
+
+    document.getElementById("european").innerHTML = europeanCountries[0];
+
+}
+
