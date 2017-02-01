030abf8fbf308be0722cae25b400d5630b468a7c
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-01 15:45:46 +0000

    Deleted "Peru" and "Chile" from the list

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsConcat.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavacriptCalculusPositionsConcat.js
@@ -1,6 +1,6 @@
 ﻿var northAmericanCountries = ["United States", "Canada", "Mexico"];
 
-var southAmericanCountries = ["Colombia", "Peru", "Chile", "Bolivia", "Venezuela"];
+var southAmericanCountries = ["Colombia", "Bolivia", "Venezuela"];
 
 document.getElementById("joinCountries").innerHTML = northAmericanCountries.concat(southAmericanCountries);
 
