504cc3f163bc8d13ae74bdc518c0a0ec17b0e78a
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-01 20:50:43 +0000

    Created a function () { } that reverses the array called "name"

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusReverse.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusReverse.js
@@ -1,1 +1,9 @@
-﻿
+﻿var name = ["Elliot", "Joana", "Vlad", "Sarah", "Alexander", "Katherine"];
+
+document.getElementsByName("span").innerHTML = name;
+
+function sortingNames() {
+
+    document.getElementsByName("span").innerHTML = name.reverse();
+
+}
