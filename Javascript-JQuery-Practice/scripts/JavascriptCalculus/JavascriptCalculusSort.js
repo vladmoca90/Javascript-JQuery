2d5b220567be238cd8210da5f964832f2d560cee
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-01 19:48:01 +0000

    Created the function that calculates the sort(). For a moment it does not work

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusSort.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/JavascriptCalculusSort.js
@@ -1,1 +1,9 @@
-﻿
+﻿var name = ["Elliot", "Joana", "Vlad", "Sarah", "Alexander", "Katherine"];
+
+document.getElementById("nameSort").innerHTML = name;
+
+function sortingNames() {
+
+    document.getElementById("nameSort").innerHTML = name.sort();
+
+}
