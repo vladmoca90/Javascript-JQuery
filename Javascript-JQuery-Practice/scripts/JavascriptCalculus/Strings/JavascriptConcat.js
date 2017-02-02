afe8aa8c5c17ada3a364e243b3fbd71ffad3d478
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-02 16:09:27 +0000

    Added the code for the relevant function

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptConcat.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptConcat.js
@@ -1,3 +1,9 @@
 ﻿function calculateConcat() {
 
+    var texts = document.getElementById("textOne").innerHTML;
+
+    var cnct = texts.concat(textOne, textTwo);
+
+    document.getElementById("concatResult").innerHTML = cnct;
+
 }
