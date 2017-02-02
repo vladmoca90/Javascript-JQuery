762a0ebb5b415a6f117f8070c9b1cc643bf15115
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-02 04:40:06 +0000

    Created both of the functions

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptSubstr.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptSubstr.js
@@ -1,1 +1,9 @@
-﻿
+﻿function substrFunction() {
+
+    var pos = document.getElementById("substrTxt").innerHTML;
+
+    var sbtr = pos.substr(7, 22);
+
+    document.getElementById("substrResult").innerHTML = sbtr;
+
+}
--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptSubstring.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptSubstring.js
@@ -1,1 +1,9 @@
-﻿
+﻿function substringFunction() {
+
+    var pos = document.getElementById("substringTxt").innerHTML;
+
+    var sbstrng = pos.substring(5, 16);
+
+    document.getElementById("substringResult").innerHTML = sbstrng;
+
+}
