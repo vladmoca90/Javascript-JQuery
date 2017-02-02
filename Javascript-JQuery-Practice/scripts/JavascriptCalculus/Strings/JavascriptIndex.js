1f919bee8c169dd8f63481b92e3525d686b30647
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-02 02:58:44 +0000

    Renamed the variable as "sentence"

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptIndex.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptIndex.js
@@ -1,10 +1,10 @@
-﻿var a = "My name is Vlad and I am from Romania";
+﻿var sentence = "My name is Vlad and I am from Romania";
 
 document.getElementsByClassName("calculateIndex").innerHTML = a;
 
 function sententceIndex() {
 
-    var b = a.indexOf("a");
+    var b = sentence.indexOf("a");
 
     document.getElementsByClassName("calculateIndex").innerHTML = b;
 
