3ffa8e857313599bba27bc6066acdbf035a39f0d
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-02 02:55:22 +0000

    Created the sententceIndex() function

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptIndex.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptIndex.js
@@ -1,1 +1,11 @@
-﻿
+﻿var a = "My name is Vlad and I am from Romania";
+
+document.getElementsByClassName("calculateIndex").innerHTML = a;
+
+function sententceIndex() {
+
+    var b = a.indexOf("a");
+
+    document.getElementsByClassName("calculateIndex").innerHTML = b;
+
+}
