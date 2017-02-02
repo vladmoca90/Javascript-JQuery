9d4c13073966193581d259484fc41468a6c91946
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-02 03:27:49 +0000

    Created the function for the .lastIndexOf()

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptLastIndex.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/JavascriptLastIndex.js
@@ -1,1 +1,9 @@
-﻿
+﻿function calcTxtLastIndex() {
+
+    var pos = document.getElementById("sentenceSecondText").innerHTML;
+
+    var str = pos.lastIndexOf("34");
+
+    document.getElementById("calculateLastIndex").innerHTML = str;
+
+}
