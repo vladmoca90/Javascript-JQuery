0f18df5f28feb96c6fc1b785be42f546498d47cc
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-03 22:37:46 +0000

    Deleted some useless spaces from the formValidation.js

--- Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
+++ Javascript-JQuery-Practice/scripts/formValidation/formValidation.js
@@ -26,14 +26,13 @@ function mailValidation() {
 
     var emailValid = document.getElementsByName("mail")[0].value;
 
-    if (
-        emailValid.indexOf("@") <= 0 ||
+    if (emailValid.indexOf("@") <= 0 ||
         emailValid.indexOf("@") != emailValid.lastIndexOf("@") ||
         emailValid.lastIndexOf(".") <= (emailValid.indexOf("@") + 2) ||
         emailValid.lastIndexOf(".") > (emailValid.length - 3) ||
         emailValid.lastIndexOf(".") < (emailValid.length - 5) ||
-        emailValid.indexOf(" ") >= 0
-    ) {
+        emailValid.indexOf(" ") >= 0)
+    {
         alert("Email is not valid!");
     }
     else {
@@ -67,7 +66,6 @@ function ageValidation() {
     if (age >= 18) {
         alert("Good! You are over 18.");
     }
-
     else {
         document.getElementsByTagName("span")[4].innerHTML = "You must be over 18 to complete the form!";
     }
