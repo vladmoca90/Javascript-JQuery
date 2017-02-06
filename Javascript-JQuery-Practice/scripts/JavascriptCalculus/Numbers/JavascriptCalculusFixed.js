db23263f13deede70ac901b1faf16c6d4f41c162
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-06 15:20:36 +0000

    Added the codes for both toFixed() and toPrecision()

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/JavascriptCalculusFixed.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/JavascriptCalculusFixed.js
@@ -1,1 +1,9 @@
-﻿
+﻿function toFixedNo() {
+
+    var presc = document.getElementById("toFixedNumber").innerHTML;
+
+    var calculateFixed = presc.toFixed(12);
+
+    document.getElementById("toFixed").innerHTML = calculateFixed;
+
+}
--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/JavascriptCalculusPrecision.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/JavascriptCalculusPrecision.js
@@ -1,1 +1,9 @@
-﻿
+﻿function toPrecisionNo() {
+
+    var presc = document.getElementById("toPrecisionNumber").innerHTML;
+
+    var calculatePresc = presc.toPrecision(7);
+
+    document.getElementById("toPrecision").innerHTML = calculatePresc;
+
+}
