9c81a878a1fa7b0097739257d96d786bdbf01e5e
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-07 15:43:29 +0000

    The function does not show the result when click on the button

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/JavascriptCalcFunction.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/JavascriptCalcFunction.js
@@ -2,15 +2,15 @@
 
     var a = [(x + 1) / (x - 3)];
 
-    var x = 3;
+    var x = 2;
 
     if (x == 3) {
         console.log("ERROR! A fraction cannot have the denominater 0.");
     }
     else {
-        console.log(x);
+        console.log(a);
     }
 
-    document.getElementById("result").innerHTML = x;
+    document.getElementById("result").innerHTML = a;
 
 }
