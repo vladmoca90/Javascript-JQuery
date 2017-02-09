45a40159eb7fb2cd3f5fd067ff41f402a6359751
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-09 23:49:30 +0000

    Created the function for the roots

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionRoots.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionRoots.js
@@ -1,5 +1,19 @@
 ﻿function calcLogRoots() {
 
+    delta = 64;
 
+    a = 5;
+
+    b = 2;
+
+    c = -3;
+
+    x = [- b + Math.sqrt(64)] / 2 * a;
+
+    y = [-b - Math.sqrt(64)] / 2 * a;
+
+    document.getElementById("rootOne").innerHTML = x;
+
+    document.getElementById("rootTwo").innerHTML = y;
 
 }
