53734c8708fc16d4b7cedcbe22337bf525bb7c35
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-10 17:46:35 +0000

    Deleted the JavascriptLogarithmFunctionRootsConditions.js

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionRoots.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionRoots.js
@@ -16,4 +16,11 @@
 
     document.getElementById("rootTwo").innerHTML = scnd;
 
+    if (frst <= 0 || scnd <= 0) {
+        document.getElementsByClassName("rootCondition").innerHTML = "Not a valid root. It is <= 0)";
+    }
+    else {
+        document.getElementsByClassName("rootCondition").innerHTML = "";
+    }
+
 }
