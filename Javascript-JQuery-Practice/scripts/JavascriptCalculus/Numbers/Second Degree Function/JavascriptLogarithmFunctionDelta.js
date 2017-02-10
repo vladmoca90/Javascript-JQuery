8cdb01c7ef19428676425aa71c5a0e4ad9bea1cd
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-10 20:21:03 +0000

    Added the delta conditions everywhere

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionDelta.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionDelta.js
@@ -11,3 +11,17 @@
     document.getElementById("delta").innerHTML = delta;
 
 }
+
+/* 
+
+if (delta > 0) {
+    alert("The roots are distinctive");
+}
+else if(delta == 0) {
+    alert("The roots are equal");
+}
+else (delta < 0) {
+    alert("The equation is imposible");
+}
+
+*/
