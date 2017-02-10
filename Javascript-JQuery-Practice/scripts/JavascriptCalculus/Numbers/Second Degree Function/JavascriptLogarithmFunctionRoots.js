a697125bbe0915eedf90e9b2f3f77b62521b77d5
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-10 17:43:01 +0000

    Created JavascriptLogarithmFunctionRootsConditions.js and put its <script> in the <head>

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionRoots.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptLogarithmFunctionRoots.js
@@ -12,13 +12,6 @@
 
     var scnd = (-b - delta) / 2 * a;
 
-    if (frst <= 0 || scnd <= 0) {
-        document.getElementsByClassName("rootCondition").innerHTML = "Not a valid root. It is <= 0)";
-    }
-    else {
-        document.getElementsByClassName("rootCondition").innerHTML = "";
-    }
-
     document.getElementById("rootOne").innerHTML = frst;
 
     document.getElementById("rootTwo").innerHTML = scnd;
