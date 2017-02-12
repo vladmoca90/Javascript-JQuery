3217e9b9118891601b7ec7843336127f743cecdc
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-12 22:23:06 +0000

    Calculated the y1 and y2 first

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptExponentialFunctionRoots.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptExponentialFunctionRoots.js
@@ -1,3 +1,23 @@
 ﻿function calcRoots() {
 
+    delta = Math.sqrt(5);
+
+    a = 1;
+
+    b = -3;
+
+    c = -4;
+
+    var yOne = (-b + delta) / 2 * a;
+
+    var frst = yOne.toFixed(2);
+
+    var yTwo = (-b - delta) / 2 * a;
+
+    var scnd = yTwo.toFixed(2);
+
+    document.getElementById("rootOne").innerHTML = frst;
+
+    document.getElementById("rootTwo").innerHTML = scnd;
+
 }
