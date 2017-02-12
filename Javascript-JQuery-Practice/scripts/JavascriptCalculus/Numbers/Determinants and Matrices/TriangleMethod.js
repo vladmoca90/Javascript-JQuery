5869488f9226437c3a1fcaa708abd7dc7ceb6b14
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-12 01:01:20 +0000

    Gave some numerical values to each letter. The determinant is equal to 46

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Determinants and Matrices/TriangleMethod.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Determinants and Matrices/TriangleMethod.js
@@ -1,1 +1,12 @@
-﻿
+﻿function calcTrian() {
+
+    a = 0; b = -1; c = 0;
+
+    d = -1; e = 0; f = 7;
+
+    g = -5; h = 16; i = -11;
+
+    x = a*e*i + d*h*c + g*b*f - c*e*g - a*f*h - d*b*i;
+
+    document.getElementById("result").innerHTML = x;
+}
