e2af1b3ba49db37a20be0e1c96aa8f3ca8c2ff85
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-12 23:46:23 +0000

    The x1 and x2 have been calculated

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptExponentialFunctionXRoots.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptExponentialFunctionXRoots.js
@@ -1,5 +1,11 @@
 ﻿function calcXRoots() {
 
+    var frst = [Math.log(4)] / [Math.log(3)];
 
+    var scnd = [Math.log(-1)] / [Math.log(3)];
+
+    document.getElementById("rootXOne").innerHTML = frst;
+
+    document.getElementById("rootXTwo").innerHTML = scnd;
 
 }
