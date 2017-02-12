301556fb02c688a0736b4d2e443037f18e06c2bb
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-12 22:27:02 +0000

    Added "Y" everywhere to make the difference

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Second Degree Function/JavascriptExponentialFunctionYRoots.js
@@ -0,0 +1,23 @@
+﻿function calcYRoots() {
+
+    delta = Math.sqrt(25);
+
+    a = 1;
+
+    b = -3;
+
+    c = -4;
+
+    var yOne = (-b + delta) / 2 * a;
+
+    var frst = yOne;
+
+    var yTwo = (-b - delta) / 2 * a;
+
+    var scnd = yTwo;
+
+    document.getElementById("rootYOne").innerHTML = frst;
+
+    document.getElementById("rootYTwo").innerHTML = scnd;
+
+}
