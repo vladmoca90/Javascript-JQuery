efb62b6e9f8ac19229702cf2789804a6523691a1
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-20 04:04:21 +0000

    Corrected declaration syntax

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Determinants and Matrices/SarusRule.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Determinants and Matrices/SarusRule.js
@@ -1,11 +1,5 @@
 ﻿function sarusRule() {
 
-    a = 1; b = -5; c = 2;
-
-    d = -1; e = 0; f = -4;
-
-    g = 1; h = -2; i = 6;
-
     y = a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h;
 
     document.getElementById("result").innerHTML = y;
