d5c78ab950ab525b325a2e675c57da14703b942f
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-22 17:40:55 +0000

    The animation works

--- Javascript-JQuery-Practice/scripts/Animation/animation.js
+++ Javascript-JQuery-Practice/scripts/Animation/animation.js
@@ -1,1 +1,32 @@
-﻿
+﻿$(document).ready(function () {
+
+    $("#parent").addClass("center-block").width(400).height(400).css({
+        "background": "yellow",
+    });
+
+    $("#child").width(150).height(150).css({
+        "background": "red",
+    });
+
+    $("#parent, #child").css("border", "1px solid #000000");
+
+    $("#button").click(function() {
+
+        $("#parent").animate({
+            background: "red",
+            width: "500px",
+            height: "500px",
+            opacity: "0.5"
+        });
+
+        $("#child").animate({
+            background: "yellow",
+            width: "75px",
+            height: "75px",
+            top: 100,
+            left: 140
+        });
+
+    });
+
+});
