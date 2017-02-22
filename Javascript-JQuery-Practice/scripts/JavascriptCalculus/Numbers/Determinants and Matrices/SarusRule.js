d623b14393747bdccea0f5c592fbc8d53621c50b
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-22 15:03:56 +0000

    Wrote the Sarul Rule project with jquery and deleted the <br /> from both determinants projects

--- Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Determinants and Matrices/SarusRule.js
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/Determinants and Matrices/SarusRule.js
@@ -1,6 +1,29 @@
-﻿function sarusRule() {
+﻿$(document).ready(function () {
 
-    y = a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h;
+    $("#sarusRule").click(function () {
 
-    document.getElementById("result").innerHTML = y;
-}
+        var a = $("#a").val();
+
+        var b = $("#b").val();
+
+        var c = $("#c").val();
+
+        var d = $("#d").val();
+
+        var e = $("#e").val();
+
+        var f = $("#f").val();
+
+        var g = $("#g").val();
+
+        var h = $("#h").val();
+
+        var i = $("#i").val();
+
+        var y = a * e * i + b * f * g + c * d * h - c * e * g - b * d * i - a * f * h;
+
+        document.getElementById("result").innerHTML = y;
+
+    });
+
+});
