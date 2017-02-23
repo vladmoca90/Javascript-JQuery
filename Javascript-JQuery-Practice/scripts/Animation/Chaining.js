3f9bc98a5b94e5e5b48da489bcf421ae6969b828
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-23 22:26:27 +0000

    The animation works

--- Javascript-JQuery-Practice/scripts/Animation/Chaining.js
+++ Javascript-JQuery-Practice/scripts/Animation/Chaining.js
@@ -1,10 +1,22 @@
 ﻿$(document).ready(function () {
+
     $("#chaining").width(400).height(400).css({
         "border": "1px solid black",
         "background": "red"
     });
 
-    $("button").onclick(function () {
-        $("#chaining").animate();
+    $("#button").click(function () {
+        $("#chaining")
+        .animate({
+            width: "325px",
+            height: "325px"
+        })
+       .animate({
+           borderRadius: "100%"
+       })
+       .animate({
+           marginTop: "100px"
+        });
     });
+
 });
