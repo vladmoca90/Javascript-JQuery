a929109e0c6ce3b7e6e2054fd81bd4e4ee4bde7a
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-23 21:33:26 +0000

    Created the csspart of the Chaining.html in jQuery

--- Javascript-JQuery-Practice/scripts/Animation/Chaining.js
+++ Javascript-JQuery-Practice/scripts/Animation/Chaining.js
@@ -1,1 +1,15 @@
-﻿
+﻿$(document).ready(function () {
+    $("#chaining").width(400).height(400).css({
+        "border": "1px solid black",
+        "background": "red"
+    });
+
+    $("#chain-interior").width(300).height(300).css({
+        "border": "1px solid black",
+        "background": "white"
+    });
+
+    $("button").onclick(function () {
+
+    });
+});
