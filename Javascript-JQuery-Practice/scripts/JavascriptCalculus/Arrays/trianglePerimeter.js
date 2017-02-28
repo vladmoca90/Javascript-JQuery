81bcd22a6b102d0b986a9fa52bc1cf2160e83ef7
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:25:45 +0000

    The perimeter of a triangle

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/trianglePerimeter.js
@@ -0,0 +1,8 @@
+﻿//The parameter is an array with 3 elements that present the sides of a triangle
+//Return the perimeter of the triangle
+
+function calculatePerimeter(array) {
+
+    return array[0] + array[1] + array[2];
+
+}
