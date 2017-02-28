d4a6fef209f25d62a9ef47d4384ece297a72add9
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:27:57 +0000

    Added two js files that contain strings with codes that calculate the reverse of a text and the number of vowels

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/numberOfVowels.js
@@ -0,0 +1,22 @@
+﻿//write a function that, given a text, returns the number of vowels in the text
+
+function countVowels(text) {
+
+    var count = 0;
+
+    for (i = 0; i < text.length; i++) {
+        var c = text.charAt(i);
+        if (isVowel(c)) {
+            count++;
+        }
+    }
+
+    return count;
+
+}
+
+function isVowel(c) {
+    var v = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    return v;
+}
