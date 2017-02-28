e1b169819e819ebe441d1c55ab06c5e6aa51792d
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:22:24 +0000

    Reverse of an array

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/reverseOfArray.js
@@ -0,0 +1,14 @@
+﻿//Given an array, return the reverse of the array
+
+function reverse(array) {
+
+    for (i = 0, j = array.length - 1; i < array.length / 2 && j >= array.length / 2; i++, j--) {
+        var a = array[i];
+        var b = array[j];
+
+        array[i] = b;
+        array[j] = a;
+    }
+
+    return array;
+}
