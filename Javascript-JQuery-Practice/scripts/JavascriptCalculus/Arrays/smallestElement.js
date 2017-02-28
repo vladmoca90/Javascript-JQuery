7750e96c3ec20ba5083da24f45f8c2c5b47f978a
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:22:59 +0000

    Smallest elements in an array

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/smallestElement.js
@@ -0,0 +1,22 @@
+﻿//smallest element in an array//
+
+function min(array) {
+
+    if (array.length == 0) {
+        throw new Error("The list cannot be empty");
+    }
+    if (array.length == 1) {
+        return array[0];
+    }
+
+    var minSoFar = array[0];
+
+    for (var i = 0; i < array.length; i++) {
+
+        if (array[i] < minSoFar) {
+            minSoFar = array[i];
+        }
+    }
+
+    return minSoFar;
+}
