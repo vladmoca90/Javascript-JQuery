180ecaf24be9970c223fe92df59c9e37acf62c90
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:25:16 +0000

    Sum of elements in an array

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/sumOfElementsInAnArray.js
@@ -0,0 +1,20 @@
+﻿//sum of the elements of an array//
+
+function sum(array) {
+
+    if (array.length == 0) {
+        throw new Error("The array is empty");
+    }
+    if (array.length == 1) {
+        if (array[0])
+            return array[0];
+    }
+
+    var total = array[0];
+
+    for (i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+
+    return total;
+}
