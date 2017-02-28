3d7b3927804e5cf6153a5ab2b8d99c6571dba934
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:45:08 +0000

    Created a js file with the factorial number

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Numbers/factorial.js
@@ -0,0 +1,12 @@
+﻿function fact(n) {
+
+    if (n < 0) {
+        throw new Error("the n must be greater of equal to 0");
+    }
+    if (n == 0) {
+        return 1;
+    }
+
+    return n * fact(n - 1);
+
+}
