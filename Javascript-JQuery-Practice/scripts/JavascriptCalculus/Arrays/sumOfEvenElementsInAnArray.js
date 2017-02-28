b516f1720cf0c7dd0bc232f77c414b3b83581033
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:25:31 +0000

    Sum of even elements in an array

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/sumOfEvenElementsInAnArray.js
@@ -0,0 +1,26 @@
+﻿//sum of all even elements in an array//
+
+function evenSum(array) {
+
+    if (array.length == 0) {
+        throw new Error("The array is empty");
+    }
+    if (array.length == 1) {
+        if (array[0] % 2 == 0) {
+            return array[0];
+        }
+        else {
+            throw new Error("The only number in the array is not even");
+        }
+    }
+
+    var even = 0;
+
+    for (i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            even += array[i];
+        }
+    }
+
+    return even;
+}
