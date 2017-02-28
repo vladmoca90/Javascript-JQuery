d8b40d07cebe0d8e893f61d22c71c7b7cc492828
Vlad Mocanu <vladmoca90@gmail.com>
2017-02-28 23:22:47 +0000

    Search an element in an array

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/searchInArray.js
@@ -0,0 +1,23 @@
+﻿//search the element in an array//
+
+function search(array, n) {
+    //validations//
+    if (array.length == 0) {
+        return false;
+    }
+    if (array.length == 1) {
+        if (n == array[0]) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] == n) {
+            return true;
+        }
+    }
+
+    return false;
+}
