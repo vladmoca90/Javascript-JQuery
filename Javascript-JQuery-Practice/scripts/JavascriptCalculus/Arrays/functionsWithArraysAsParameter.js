1543e154d69e8e65b37382706db7af592b151b23
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-07 19:01:16 +0000

    Created a js file with arrays as parameters

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/functionsWithArraysAsParameter.js
@@ -0,0 +1,42 @@
+﻿//Write the following functions, each of them takes an array as a parameter
+//head([]): returns the first element of the array
+//tail([]): returns the array without the first element
+//last([]): returns the last element of the array 
+
+function head(array) {
+
+    if (array.length == 0) {
+        throw new Error("The array is empty");
+    }
+    if (array.length == 1) {
+        return array[0];
+    }
+
+    return array[0];
+}
+
+function tail(array) {
+
+    if (array.length == 0) {
+        throw new Error("The array is empty");
+    }
+    if (array.length == 1) {
+        return [];
+    }
+
+    var removeFirst = array.shift();
+
+    return array;
+}
+
+function last(array) {
+
+    if (array.length == 0) {
+        throw new Error("The array is empty");
+    }
+    if (array.length == 1) {
+        return array[0];
+    }
+
+    return array.pop();
+}
