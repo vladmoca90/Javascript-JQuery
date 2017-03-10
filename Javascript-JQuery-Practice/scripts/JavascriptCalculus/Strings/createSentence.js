2e71da5d0aab690311949578556450128e8acc7f
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-10 20:35:37 +0000

    Created four more js codes

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Strings/createSentence.js
@@ -0,0 +1,25 @@
+﻿//A function receives a list of words and return a string with all the words, 
+//but are separated through a space, and you have "." at the end
+
+function createdSentence(array) {
+
+    if (array.length == 0) {
+        throw new Error("The array is empty");
+    }
+    if (array.length == 1) {
+        return array[0] + ".";
+    }
+
+    var combine = "";
+
+    for (var i = 0; i < array.length; i++) {
+        if (i < array.length - 1) {
+            combine += array[i] + " ";
+        }
+        else {
+            combine += array[i];
+        }
+    }
+
+    return combine + ".";
+}
