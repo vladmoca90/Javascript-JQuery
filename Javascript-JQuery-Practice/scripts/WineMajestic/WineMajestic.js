d24413dca5462710a01e63353693232c31fe610b
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-15 22:12:54 +0000

    Added the .html, .scss and .js files for the WineMajestic project

---
+++ Javascript-JQuery-Practice/scripts/WineMajestic/WineMajestic.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+
+    $(".like-button").click(function () {
+
+    });
+
+    $(".send-button").click(function () {
+
+        var container = $("#template").clone();
+
+        container.find(".text").html($("#send-container input").val());
+
+        container.attr("id", null);
+
+        var li = $("<li></li>");
+
+        li.append(container);
+
+        $("ul").append(li);
+
+        $("#send-container input").val("");
+
+    });
+
+});
