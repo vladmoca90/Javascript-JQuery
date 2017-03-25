ac81b777a46efa3dadb77ff947a38145023682c8
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-25 12:38:25 +0000

    Added some styles for #text-container and #text

--- Javascript-JQuery-Practice/scripts/columnsJQuery/columnsJQuery.js
+++ Javascript-JQuery-Practice/scripts/columnsJQuery/columnsJQuery.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(function () {
+    $("#text-container").width(650).css({
+        "border": "1px solid #000",
+        "margin": "auto"
+    });
+    $("#text").css({
+        "column-count": "3",
+        "column-gap": "15px",
+        "column-style": "1px dotted #000"
+    });
+});
