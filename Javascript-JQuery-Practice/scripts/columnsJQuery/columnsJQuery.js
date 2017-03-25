d00e2ced800f31171cbf91618962019256094da3
Vlad Mocanu <vladmoca90@gmail.com>
2017-03-25 12:49:38 +0000

    Created the container and the columns for the #text

--- Javascript-JQuery-Practice/scripts/columnsJQuery/columnsJQuery.js
+++ Javascript-JQuery-Practice/scripts/columnsJQuery/columnsJQuery.js
@@ -1,11 +1,12 @@
 ﻿$(function () {
-    $("#text-container").width(650).css({
+    $("#text-container").width(800).css({
         "border": "1px solid #000",
-        "margin": "auto"
+        "margin": "2% auto"
     });
     $("#text").css({
         "column-count": "3",
         "column-gap": "15px",
-        "column-style": "1px dotted #000"
+        "column-rule": "1px dotted #000",
+        "padding": "15px"
     });
 });
