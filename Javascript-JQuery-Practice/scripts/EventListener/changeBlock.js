a3bad9a6bf564313c5111a914aaa42bcd7e1754e
Vlad Mocanu <vladmoca90@gmail.com>
2017-04-17 00:58:35 +0100

    Added an eventListener to my function

--- Javascript-JQuery-Practice/scripts/EventListener/changeBlock.js
+++ Javascript-JQuery-Practice/scripts/EventListener/changeBlock.js
@@ -8,6 +8,18 @@
 
     scndBlock = "display: none";
 
+    document.getElementsByTagName("button").addEventListener("click", function () {
+
+        var firstBlock = document.getElementsByClassName("first").style;
+
+        firstBlock = "display: none";
+
+        var scndBlock = document.getElementsByClassName("scnd").style;
+
+        scndBlock = "display: block";
+
+    });
+
 }
 
 changeBlock();
