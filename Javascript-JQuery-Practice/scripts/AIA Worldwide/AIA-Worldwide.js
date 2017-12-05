42c83267563f1614163770b509a58f5c9d0986fc
Vlad Mocanu <vladmoca90@gmail.com>
2017-12-05 01:47:06 +0000

    Created the files for the AIA Worldwide test

---
+++ Javascript-JQuery-Practice/scripts/AIA Worldwide/AIA-Worldwide.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    function onTabClick(event) {
+        var actives = document.querySelectorAll('.active');
+
+        // deactivate existing active tab and panel
+        for (var i = 0; i < actives.length; i++) {
+            actives[i].className = actives[i].className.replace('active', '');
+        }
+
+        // activate new tab and panel
+        event.target.parentElement.className += ' active';
+        document.getElementById(event.target.href.split('#')[1]).className += ' active';
+    }
+
+    var el = document.getElementById('nav-tab');
+
+    el.addEventListener('click', onTabClick, false);
+})();
