f40b4e51598c4ae892bf43093daa0a9d373e14fa
Vlad Mocanu <vladmoca90@gmail.com>
2018-02-18 11:35:17 +0000

    Created a file for the sl;ice of an array

---
+++ Javascript-JQuery-Practice/scripts/JavascriptCalculus/Arrays/Slice.js
@@ -0,0 +1,11 @@
+﻿var fruits = ["Banana", "Orange", "Apple", "Mango", "Apricot", "Fig"];
+
+document.getElementById("practice").innerHTML = fruits;
+
+function myFunction() {
+
+    fruits.slice(1, 1, "Kiwi");
+
+    document.getElementById("practice").innerHTML = fruits;
+
+}
