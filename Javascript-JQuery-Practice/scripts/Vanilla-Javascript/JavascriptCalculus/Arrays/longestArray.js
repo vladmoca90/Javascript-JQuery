18acf771a69c2dbd579e504a5f5ae7b0aba5b3f7
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-16 17:16:39 +0000

    Created a code that compares two arrays and returns the longest one

---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/longestArray.js
@@ -0,0 +1,18 @@
+ //A function that compares the length of two arrays and returns the longest one
+
+ function longestArray(x, y) {
+
+  var longest = [];
+
+  if (x.length > y.length) {
+     return x.concat(longest);
+  } /*else if(x.length == y.length) {
+   throw new Error("The arrays are equal");
+  } */
+  else {
+     return y.concat(longest);
+  }
+
+  return longest;
+
+ }
