daf1745c3dd466e6a7ae07fe4abe70a4aa8c3373
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-16 22:44:07 +0000

    Created a code in which returns true if all elements in an array are even

---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/allElementsAreEven.js
@@ -0,0 +1,24 @@
+//A function that returns true if all elements in an array are even and false otherwise
+
+function allElementsAreEven(array) {
+
+ if (array.length == 0) {
+  return false;
+ }
+ if (array.length == 1) {
+  if (array[0] % 2 == 0) {
+   return true;
+  } else {
+   return false;
+  }
+ }
+
+ for (i = 0; i < array.length; i++) {
+  if(array[i] % 2 != 0) {
+   return false;
+  }
+ }
+
+ return true;
+
+}
