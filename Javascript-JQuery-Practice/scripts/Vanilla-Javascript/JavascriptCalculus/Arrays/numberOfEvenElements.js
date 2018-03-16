57891b0486c800c8f4488c66c49aa03c02431978
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-16 23:40:26 +0000

    A function that calculates the number of even numbers in an array

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/numberOfEvenElements.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/numberOfEvenElements.js
@@ -1,5 +1,26 @@
 //A function that returns the number of even elements in an array
 
-function numberOfEvenElements() {
+function numberOfEvenElements(array) {
+
+ if (array.length == 0) {
+  throw new Error("The array is empty");
+ }
+ if (array.length == 1) {
+  if (array[0] % 2 == 0) {
+   return array.length;
+  } else {
+   return 0;
+  }
+ }
+
+ var count = 0;
+
+ for (i = 0; i < array.length; i++) {
+  if (array[i] % 2 == 0) {
+   count++;
+  }
+ }
+
+ return count;
 
 }
