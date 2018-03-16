beaca7d8440e706792f0269812e2e4cefa39c190
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-16 14:04:52 +0000

    Rewrote the code for a fixed and precise number

---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Numbers/fixedNumber.js
@@ -0,0 +1,14 @@
+﻿//Calculate the fixed value of a given numer
+
+function fixedNumber(n) {
+
+if(n < 0) {
+    throw new Error("Then number must be greater than 0");
+}
+if(n == 0) {
+    return 0;
+}
+
+return n.toFixed();
+
+}
---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Numbers/preciseNumber.js
@@ -0,0 +1,14 @@
+﻿//Calculate the precise value of a given numer
+
+function preciseNumber(n) {
+
+if(n < 0) {
+    throw new Error("Then number must be greater than 0");
+}
+if(n == 0) {
+    return 0;
+}
+
+return n.toPrecision();
+
+}
