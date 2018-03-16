5371594d35bc3cb8a5d4aa29a5ea289d5ded71d1
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-16 16:51:36 +0000

    Renamed some ids
    Started to rewrite the code

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/Slice.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/Slice.js
@@ -1,11 +1,11 @@
 ﻿var fruits = ["Banana", "Orange", "Apple", "Mango", "Apricot", "Fig"];
 
-document.getElementById("practice").innerHTML = fruits;
+document.getElementById("sliceArray").innerHTML = fruits;
 
 function myFunction() {
 
     fruits.slice(2, 3, "Kiwi");
 
-    document.getElementById("practice").innerHTML = fruits;
+    document.getElementById("sliceArray").innerHTML = fruits;
 
 }
