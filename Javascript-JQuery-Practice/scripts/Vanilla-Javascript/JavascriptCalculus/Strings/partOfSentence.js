544595c458b498693becd1c4bfeafd365165861a
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-16 01:33:55 +0000

    Deleted the empty string

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Strings/partOfSentence.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Strings/partOfSentence.js
@@ -1,8 +1,6 @@
 //Return a portion of an array
 
-function partOfSentence() {
-
- var sentence = "My name is Vlad Mocanu";
+function partOfSentence(sentence) {
 
  return sentence.substring(0, 10);
 
