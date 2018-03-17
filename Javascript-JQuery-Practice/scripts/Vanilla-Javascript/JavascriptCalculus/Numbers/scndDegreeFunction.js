64037ddfeb4bb9ec3b906ec3bad3247fd5c6f0c9
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-17 23:17:49 +0000

    Created the function

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Numbers/scndDegreeFunction.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Numbers/scndDegreeFunction.js
@@ -2,8 +2,26 @@
 
 function secondDgr(a, b, c) {
 
-var ecuation = a*Math.pow(x, 2) + b*x + c;
+    var ecuation = a * Math.pow(x, 2) + b * x + c;
 
-var delta = Math.pow(b, 2) - 4*a*c;
+    var roots = [x1, x2];
 
+    var delta = Math.pow(b, 2) - 4 * a * c;
+
+    x1 = [b - Math.sqrt(delta)] / (2 * a);
+
+    x2 = -[b - Math.sqrt(delta)] / (2 * a);
+
+
+    if (delta < 0) {
+        throw new Error("equation is impossible");
+    }
+    if (delta == 0) {
+        return x1 == x2;
+    }
+    if (delta > 0) {
+        return x1 != x2;
+    }
+
+    return roots;
 }
