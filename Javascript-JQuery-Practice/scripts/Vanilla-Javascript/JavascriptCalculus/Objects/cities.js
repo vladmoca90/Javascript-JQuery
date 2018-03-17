d512e193d2255d4bcfc2b7f9f912bcf4ce0308b2
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-17 20:06:35 +0000

    Changed the object a bit

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Objects/cities.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Objects/cities.js
@@ -1,21 +1,28 @@
-//An object that is named cities and has 2 properties
-//The properties are name and state
-//The function allCities selects the propery city.name
+//An object that is named city and has the property name
+//The function cities() analyzes the cases
 
-var cities = {
-  name: ["New Yrk City", "Los Angeles", "Atlanta"],
-  state: ["New York", "California", "Georgia"],
+var city = {
+  name: [
+    "New York City",
+    "Los Angeles",
+    "Atlanta"
+  ],
+    state: [
+      "New York",
+      "California",
+      "Georgia",
+    ],
 }
 
-function allCities(cities) {
+function cities(city) {
 
   switch (city.name) {
 
     case "New York City":
-      return "The city is called New York City";
+      return console.log("The city is called New York City");
     case "Los Angeles":
-      return "This californian city is Los Angeles";
+      return console.log("This californian city is Los Angeles");
     case "Atlanta":
-      return "Atlanta is the capital of Georgia state";
+      return console.log("Atlanta is the capital of Georgia state");
   }
 }
