a115379af3ff0da6e35d8ac3f4b4ba17298d7346
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 17:04:00 +0000

    Created a function that returns true is the length of an array is odd

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/arrayLength-Even-Odd.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/arrayLength-Even-Odd.js
@@ -13,3 +13,19 @@ function arrayLengthIsEven(array) {
     return true;
 
 }
+
+//A function that returns true if the length of an array is odd
+
+function arrayLengthIsOdd(array) {
+
+    if(array.length == 0) {
+        throw new Error("The array is empty");
+    }
+
+    if(array.length % 2 == 0) {
+        return false;
+    }
+
+    return true;
+
+}
