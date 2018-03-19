10dc4308c8e26f77c9d9dd9b6c121b62d82f10dc
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 19:20:41 +0000

    Wrote the basis of a function that returns the difference between two arrays

---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/differenceBetweenArrays.js
@@ -0,0 +1,31 @@
+//A function that finds the difference between two arrays and returns a new array with the diferences
+//Both arrays have 3 elements
+
+/*
+function findDifference(array1, array2) {
+
+    var difArray = [];
+
+    array1 = [a, b, c];
+
+    a = array1[0];
+
+    b = array1[1];
+
+    c = array1[2];
+
+    array2 = [d, e, f];
+
+    d = array2[0];
+
+    e = array2[1];
+
+    f = array2[2];
+
+    if (array1.length == 0 || array2.length == 0) {
+        throw new Error("None of the arrays can be empty");
+    }
+
+    return difArray;
+
+} */
