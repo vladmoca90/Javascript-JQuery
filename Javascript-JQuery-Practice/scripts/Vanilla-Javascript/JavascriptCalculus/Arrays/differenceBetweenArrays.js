d429b35e7fe5d8dd23a6956b27865270d955bf36
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 21:17:39 +0000

    The code works

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/differenceBetweenArrays.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/differenceBetweenArrays.js
@@ -5,17 +5,52 @@ function findDifference(array1, array2) {
 
     var difArray = [];
 
-     if (array1.length == 0 || array2.length == 0) {
+    if (array1.length == 0 || array2.length == 0) {
         throw new Error("None of the arrays can be empty");
     }
 
-    for (i = 0, j = 0; i < array1[i].length, j < array2[j].length; i++, j++) {
-        if(array1[i] == array2[j]) {
-            return difArray;
+    if (array1.length == 1 && array2.length == 1) {
+        if (array1[0] == array2[0]) {
+            return diffArray;
         }
-        if(array1[i] != array2[j]) {
-            return difArray.push(i);
+        if (array1[0] != array2[0]) {
+            return array1.concat(array2);
         }
+    }
+
+    for (i = 0; i < array1.length; i++) {
+        var isInSecondArray = false;
+
+        var isInFirstArray = false;
+
+        for (j = 0; j < array2.length; j++) {
+            if (array1[i] == array2[j]) {
+                isInSecondArray = true;
+                break;
+            }
+        }
+
+        if (!isInSecondArray) {
+            difArray.push(array1[i]);
+        }
+
+    }
+
+     for (j = 0;j < array2.length; j++) {
+
+        var isInFirstArray = false;
+
+        for (i = 0; i < array1.length; i++) {
+            if (array2[j] == array1[i]) {
+                isInFirstArray = true;
+                break;
+            }
+        }
+
+        if (!isInFirstArray) {
+            difArray.push(array2[j]);
+        }
+
     }
 
     return difArray;
