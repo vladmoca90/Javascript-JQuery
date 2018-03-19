292d6495a421e5ccd33c1fa5c51cc3456fbce1a8
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 17:36:59 +0000

    A function that matches a string with the element of a given object

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Strings/matchString.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Strings/matchString.js
@@ -3,11 +3,34 @@
 
 function matchString(string1, string2) {
 
-    if(string1.length == 0 || string2.length == 0) {
+    if (string1.length == 0 || string2.length == 0) {
         throw new Error("None of the two strings can be empty");
     }
 
-    if(string1.match(string2)) {
+    if (string1.match(string2)) {
+        return true;
+    }
+
+    return false;
+
+}
+
+//A function that matches a string with another string from a given object
+
+function matchStringWithObject(string) {
+
+    var person = {
+        name: "John Swift",
+        age: "25 years old",
+        position: "UX Designer",
+        worktown: "Bracknell",
+    }
+
+    if (string.length == 0) {
+        throw new Error("The string cannot be empty");
+    }
+
+    if (string.match(person.name)) {
         return true;
     }
 
