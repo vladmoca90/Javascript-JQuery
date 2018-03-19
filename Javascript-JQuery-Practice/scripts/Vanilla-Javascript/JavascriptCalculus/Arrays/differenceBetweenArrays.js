68c0e60c392e793d1ed10a444a83737906af10b6
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 19:36:25 +0000

    The difference between arrays code not working entirely

--- Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/differenceBetweenArrays.js
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Arrays/differenceBetweenArrays.js
@@ -1,31 +1,23 @@
 //A function that finds the difference between two arrays and returns a new array with the diferences
 //Both arrays have 3 elements
 
-/*
 function findDifference(array1, array2) {
 
     var difArray = [];
 
-    array1 = [a, b, c];
-
-    a = array1[0];
-
-    b = array1[1];
-
-    c = array1[2];
-
-    array2 = [d, e, f];
-
-    d = array2[0];
-
-    e = array2[1];
-
-    f = array2[2];
-
-    if (array1.length == 0 || array2.length == 0) {
+     if (array1.length == 0 || array2.length == 0) {
         throw new Error("None of the arrays can be empty");
+    }
+
+    for (i = 0, j = 0; i < array1[i].length, j < array2[j].length; i++, j++) {
+        if(array1[i] == array2[j]) {
+            return difArray;
+        }
+        if(array1[i] != array2[j]) {
+            return difArray.push(i);
+        }
     }
 
     return difArray;
 
-} */
+}
