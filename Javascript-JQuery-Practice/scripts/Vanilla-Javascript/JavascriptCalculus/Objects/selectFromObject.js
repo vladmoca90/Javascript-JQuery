4e217ebba37d70ffdc7cef3daf5aefe678a3ff73
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 17:11:24 +0000

    Created a function that selects two object properties and creates a sentence

---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Objects/selectFromObject.js
@@ -0,0 +1,13 @@
+// A function that selects an object property and returns that value
+
+function selectFromObj() {
+
+    var person = {
+        name: "John Swift",
+        age: 25,
+        position: "UX Designer",
+        worktown: "Bracknell",
+    }
+
+    return person.name + " " + "is" + " " + person.age + " ";
+}
