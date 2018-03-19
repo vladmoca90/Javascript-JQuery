01109a6a92c87b4215c4fcc6c579d990996c2ecc
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-19 21:51:59 +0000

    Created a code that returns true of the length of a number is 2

---
+++ Javascript-JQuery-Practice/scripts/Vanilla-Javascript/JavascriptCalculus/Numbers/lengthOfNumber.js
@@ -0,0 +1,11 @@
+//A function that returns the length of a number (>2)
+
+function lengthOfNumber(n) {
+
+    if(n.toString().length == 1 || n.toString().length >2) {
+        return false
+    }
+
+    return true;
+
+}
