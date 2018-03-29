eb500ab937b93b32d3f8a952370f461bfb436352
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-29 20:51:58 +0100

    Created a JSON object

--- scripts/Vanilla-Javascript/filter/filter.js
+++ scripts/Vanilla-Javascript/filter/filter.js
@@ -1,3 +1,8 @@
 function filterInput() {
-    
+
+    var words = '{ "actors" : [' +
+        '{ "heading":"Russell Crowe" , "subHeading":"7 April 1964" },' +
+        '{ "heading":"Natalie Portman" , "subHeading":"9 June 1981" },' +
+        '{ "heading":"Gerard Butler" , "subHeading":"13 November 1969" }, ]}';
+
 }
