5008465fe6cd73d74ff6c13b166b681cb4541a57
Vlad Mocanu <vladmoca90@gmail.com>
2018-03-29 21:33:03 +0100

    Deleted

--- scripts/Vanilla-Javascript/filter/filter.js
+++
@@ -1,8 +0,0 @@
-function filterInput() {
-
-    var words = '{ "actors" : [' +
-        '{ "heading":"Russell Crowe", "subHeading":"7 April 1964", "subHeadingSmall":"Wellington, New Zealand" },' +
-        '{ "heading":"Natalie Portman", "subHeading":"9 June 1981" }, "subHeadingSmall":"Jerusalem, Israel"' +
-        '{ "heading":"Gerard Butler", "subHeading":"13 November 1969" }, "subHeadingSmall":"Paisley, Scotland" ]}';
-
-}
