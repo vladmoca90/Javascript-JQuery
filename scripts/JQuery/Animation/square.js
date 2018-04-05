4ebdfa5b8082550f1914cb4037f743509f0c54cb
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-06 00:15:46 +0100

    Added .scss to the square

--- scripts/JQuery/Animation/square.js
+++ scripts/JQuery/Animation/square.js
@@ -3,21 +3,18 @@
 
     $("#button").click(function() {
 
-        $(".square").animate({
-            background: "red",
+         $(".square, .square__child").animate({
             borderRadius: "100%",
-            height: "400px",
-            marginTop: "75px",
-            width: "400px"
+        });
+
+        $(".square").animate({
+            height: "500px",
+            width: "500px"
         });
 
         $(".square__child").animate({
-            background: "yellow",
-            borderRadius: "100%",
-            display: "block",
-            height: "125px",
-            margin: "125px auto",
-            width: "125px"
+            height: "150px",
+            width: "150px"
         });
 
     });
