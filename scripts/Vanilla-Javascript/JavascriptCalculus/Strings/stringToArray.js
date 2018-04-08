b1a862d612826aac8990ffead3a6f965c8f0cced
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-08 17:27:38 +0100

    Created two more functions that split a string into an array of characters and array of words

--- scripts/Vanilla-Javascript/JavascriptCalculus/Strings/stringToArray.js
+++ scripts/Vanilla-Javascript/JavascriptCalculus/Strings/stringToArray.js
@@ -1,9 +1,33 @@
 //A function that transforms a string in an array
 
+//will make the entire string as an array
 function stringToArray(string) {
+
     if (string.length == 0) {
         throw new Error('The string cannot be empty');
     }
 
     return string.trim().split();
+}
+
+
+//will make each string character as an array element
+function stringToArray(string) {
+
+    if (string.length == 0) {
+        throw new Error('The string cannot be empty');
+    }
+
+    return string.trim().split('');
+}
+
+
+//will make each string word character as an array element
+function stringToArray(string) {
+
+    if (string.length == 0) {
+        throw new Error('The string cannot be empty');
+    }
+
+    return string.trim().split(''); //the '' needs a space between
 }
