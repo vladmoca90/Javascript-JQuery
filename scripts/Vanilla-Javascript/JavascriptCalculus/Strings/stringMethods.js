3343eb2154f3842b22663969fecca23f7616682a
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-08 23:59:09 +0100

    Another function with substr()

--- scripts/Vanilla-Javascript/JavascriptCalculus/Strings/stringMethods.js
+++ scripts/Vanilla-Javascript/JavascriptCalculus/Strings/stringMethods.js
@@ -8,3 +8,14 @@ function substringString(string, a, b) {
 
     return string.substring(a, b);
 }
+
+//A function that applies substr() to a string
+function substrString(string, a, b) {
+
+    if (string.length == 0) {
+        throw new Error('The string cannot be empty');
+    }
+
+    return string.substr(a, b);
+}
+
