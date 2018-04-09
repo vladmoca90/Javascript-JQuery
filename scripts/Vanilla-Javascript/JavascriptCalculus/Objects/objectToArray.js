3dbdd9da282a79322d3a4aa9e67f070250e3b248
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-10 00:09:57 +0100

    Created an object that becomes an array using Object.values()

--- scripts/Vanilla-Javascript/JavascriptCalculus/Objects/objectToArray.js
+++ scripts/Vanilla-Javascript/JavascriptCalculus/Objects/objectToArray.js
@@ -1,7 +1,6 @@
-//An array of objects that has the properties of cars
+//An array of objects that has the properties of cars and returns an array with specific values from the object
 
-var cars = [
-    {
+var cars = [{
         model: 'Volvo',
         country: 'Sweden',
         number: '34BR345',
@@ -22,3 +21,12 @@ var cars = [
         number: '00JY676',
     }
 ];
+
+function chooseCar(n) {
+
+    if (n < 0 || n > 2) {
+        throw new Error('The number must be 0, 1 or 2');
+    }
+
+    return Object.values(cars[n]);
+}
