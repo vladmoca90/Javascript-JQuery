1f4f8a2b45d0890b9f3025c746a775d65b471b96
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-09 23:48:55 +0100

    Finished the array of objects called cars

--- scripts/Vanilla-Javascript/JavascriptCalculus/Objects/cars.js
+++ scripts/Vanilla-Javascript/JavascriptCalculus/Objects/cars.js
@@ -2,12 +2,23 @@
 
 var cars = [
     {
-
+        model: 'Volvo',
+        country: 'Sweden',
+        number: '34BR345',
     },
     {
-
+        model: 'Honda',
+        country: 'Japan',
+        number: '06GF090',
     },
     {
-        
+        model: 'Lada',
+        country: 'Russia',
+        number: '11EE779',
+    },
+    {
+        model: 'Mercedes',
+        country: 'Germany',
+        number: '00JY676',
     }
 ];
