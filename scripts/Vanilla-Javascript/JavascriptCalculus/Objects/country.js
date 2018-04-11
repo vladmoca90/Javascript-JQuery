4d20dd3a6add3dbf18bf4a4651b27545fdb36575
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-11 21:18:32 +0100

    Deleted a folder

---
+++ scripts/Vanilla-Javascript/JavascriptCalculus/Objects/country.js
@@ -0,0 +1,11 @@
+//A object with properties of a country
+
+var country = {
+    name: "Uzbekistan",
+    population: 27, // in millions //
+    capital: "Tashkent",
+    otherCities: ["Samarkhand", "Burkhara", "Nukus"],
+    size: 447400,
+    region: "Central Asia",
+    independence: 1991,
+}
