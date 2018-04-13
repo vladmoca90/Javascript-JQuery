085212443368a826f636f4ddcd4d40dc37199817
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-14 00:34:36 +0100

    Added gulp-minify to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var min = require('gulp-minify');
 
 gulp.task('sass', function () {
   return gulp.src('content/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('content/'))
+});
+
+gulp.task('minify', function() {
+    return gulp.src('content/**/*.css')
+        .pipe(min())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('content/'));
 });
 
 // Watch task
