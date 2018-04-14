480eff814716db0b83a78ebe15d84a1d2e9b60cc
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-14 20:18:15 +0100

    Added files .min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var minifyCSS = require('gulp-minify-css');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var minify = require('gulp-minify');
 var rename = require('gulp-rename');
-
+var concat = require('gulp-concat');
+ 
 gulp.task('sass', function () {
   return gulp.src('content/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('content/'))
 });
-
+ 
 gulp.task('minify', function() {
     return gulp.src('content/**/*.css')
-        .pipe(uglify())
+        .pipe(minify())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('content/'));
 });
 
+gulp.task('concat', function() {
+    return gulp.src('content/**/*.css')
+        .pipe(concat())
+        .pipe(gulp.dest('content/'));
+});
+ 
 // Watch task
 gulp.task('default', function () {
   return gulp.watch('content/**/*.scss', 'sass');
