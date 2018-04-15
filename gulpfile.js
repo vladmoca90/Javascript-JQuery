6e877dd6cf544bbf64841dfdf036e68ff6fdfbe1
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-15 03:46:06 +0100

    ADded the new gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var minify = require('gulp-minify');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
- 
+var minifyCSS = require('gulp-minify-css');
+
 gulp.task('sass', function () {
   return gulp.src('content/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('content/'))
 });
- 
-gulp.task('minify', function() {
+
+gulp.task('minify-css', function() {
     return gulp.src('content/**/*.css')
-        .pipe(minify())
+        .pipe(minifyCSS())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('content/'));
 });
 
-gulp.task('concat', function() {
-    return gulp.src('content/**/*.css')
-        .pipe(concat())
-        .pipe(gulp.dest('content/'));
-});
- 
 // Watch task
 gulp.task('default', function () {
-  return gulp.watch('content/**/*.scss', 'sass');
+  return gulp.watch('content/**/*.scss', ['sass']);
 });
