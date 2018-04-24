a25216e40517b09fc649e3444dfd510c2d71bc29
Vlad Mocanu <vladmoca90@gmail.com>
2018-04-24 23:00:47 +0100

    Changed

--- scripts/Vanilla-Javascript/Exercises/Strings/capitalizeFirstLetter.js
+++ scripts/Vanilla-Javascript/Exercises/Strings/capitalizeFirstLetter.js
@@ -6,12 +6,16 @@ function capitalizeFirstLetter(text) {
         throw new Error('The text cannot be empty');
     }
 
+    if (text.length == 1) {
+        return text.toUpperCase();
+    }
+
     var splitText = text.trim().split(' ');
 
-    var capitalizedLetters = splitText[0].charAt(0).tuUpperCase();
+    var capitalizedLetters = splitText[0].charAt(0).toUpperCase();
 
     for (i = 0; i < splitText.length; i++) {
-        capitalizedLetters = splitText[i].toUppercase();
+        capitalizedLetters = splitText[i].charAt(0).toUpperCase();
     }
 
     return capitalizedLetters.join(' ');
